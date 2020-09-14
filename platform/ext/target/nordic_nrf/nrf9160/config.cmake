23138db5149aac6fb2ee5c61fc45c93024b04ffb
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-09-14 15:19:28 +0200

    platform: Add support for new SOCs: nRF5340 and nRF9160
    Supporting most configurations.
    
    This patch targets the nRF9160 and the nRF5340 application core.
    
    The nRF5340 is a dual-core SOC (application core and network core). The
    nRF5340 application core is a Cortex-M33F with TrustZone-M.
    
    The nRF9160 is an SOC with LTE-M/NB-IOT/GPS connectivity. The nRF9160 has
    a Cortex-M33F with TrustZone-M.
    
    Common code between the two platforms has been placed into a common
    directory.
    
    The platforms do not use the tfm_common_s.ld linker script since the nRF
    IDAU needs some alterations to the placement of the veneers.
    
    The common directory contains a copy of the nrfx driver framework,
    copied from https://github.com/NordicSemiconductor/nrfx version 2.3.0
    at commit 6c31cb7d0e4038afa1efc9bfed40206adfd7b83e.
    
    ARMCLANG and IAR support have been added but not tested.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
    Change-Id: I15b932f444aa9adf2ef2489e9ecf036c64c851a7

---
+++ platform/ext/target/nordic_nrf/nrf9160/config.cmake
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2020, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+set(SECURE_UART1 ON)
+set(PSA_API_TEST_TARGET nrf)
---
+++ platform/ext/target/nordic_nrf/nrf9160dk_nrf9160/config.cmake
@@ -0,0 +1,3 @@
+set(NRF_PATH platform/ext/target/${TFM_PLATFORM}/..)
+
+include(${NRF_PATH}/nrf9160/config.cmake)
