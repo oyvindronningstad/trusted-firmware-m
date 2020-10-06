c8707753cf0e4c46d14f89641435e0d023c25ec6
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2020-10-06 12:06:09 +0200

    fixup! define variables as CACHE in nRF platform cmake
    Declare locally-defined variables (in nRF platform cmake
    files as CACHE).
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- platform/ext/target/nordic_nrf/nrf5340/config.cmake
+++ platform/ext/target/nordic_nrf/nrf5340/config.cmake
@@ -5,6 +5,6 @@
 #
 #-------------------------------------------------------------------------------
 
-set(SECURE_UART1 ON)
-set(PSA_API_TEST_TARGET nrf)
-set(TFM_EXTRA_GENERATED_FILE_LIST_PATH ${CMAKE_CURRENT_SOURCE_DIR}/${NRF_PATH}/common/generated_file_list.yaml)
+set(SECURE_UART1                        ON         CACHE BOOL      "Enable secure UART1" FORCE)
+set(PSA_API_TEST_TARGET                 "nrf"      CACHE STRING    "PSA API test target" FORCE)
+set(TFM_EXTRA_GENERATED_FILE_LIST_PATH  ${CMAKE_CURRENT_SOURCE_DIR}/${NRF_PATH}/common/generated_file_list.yaml          CACHE PATH      "Path to extra generated file list. Appended to stardard TFM generated file list." FORCE)
